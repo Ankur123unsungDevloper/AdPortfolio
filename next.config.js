6a3528d4746b1ce6d99b288e9ab8c8eb55c75218
Ankur123unsungDevloper <ankurdas1804@gmail.com>
2026-03-26 01:06:08 +0530

    your change

--- next.config.js
+++ next.config.js
@@ -1,12 +1,9 @@
 /** @type {import('next').NextConfig} */
-
-const isProd = process.env.NODE_ENV === 'production';
-
 const nextConfig = {
   output: 'export',
   images: { unoptimized: true },
-  basePath: isProd ? '/AdProtfolio' : '',
-  assetPrefix: isProd ? '/AdProtfolio/' : '',
+  basePath: '/AdProtfolio',
+  assetPrefix: '/AdProtfolio/',
 };
 
 module.exports = nextConfig;
